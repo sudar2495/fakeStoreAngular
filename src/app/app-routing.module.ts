e0c727e6b95874142f7bb42d9119f6b7e9f0c092
sudarmani <sudarmani.ulaganathan@siemens.com>
2022-10-09 00:21:22 +0530

    dfvbdfbdv

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,9 @@ import { CalculatorComponent } from '../app/components/calculator/calculator.com
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'calculator', component: CalculatorComponent, pathMatch: 'full' }
+  { path: 'calculator', component: CalculatorComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: ''},
+
 ];
 
 @NgModule({
